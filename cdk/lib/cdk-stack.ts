a661c66a3fedc1b35a4f1a549fa45acc735124ac
randyvroegop <randy.vroegop@luminis.eu>
2023-11-14 12:58:14 +0100

    make it accessible

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,23 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Instance, Vpc, InstanceType, InstanceClass, InstanceSize, AmazonLinuxImage } from 'aws-cdk-lib/aws-ec2'
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Instance, Vpc, InstanceType, InstanceClass, InstanceSize, AmazonLinuxImage, SubnetType, SecurityGroup, Port, Peer } from 'aws-cdk-lib/aws-ec2'
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create a VPC (required for launching EC2 instance)
-    const vpc = new Vpc(this, 'VPC');
-
     // Define the EC2 instance details
     new Instance(this, 'Instance', {
-      vpc,
-      instanceType: InstanceType.of(
-        InstanceClass.T3,
-        InstanceSize.MICRO
-      ),
+      vpc: new Vpc(this, 'VPC'),
+      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+      vpcSubnets: { subnetType: SubnetType.PUBLIC },
+      associatePublicIpAddress: true,
       machineImage: new AmazonLinuxImage(),
+      ssmSessionPermissions: true
     });
   }
 }
