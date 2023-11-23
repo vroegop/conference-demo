ac2d248cae7b9fc20b5a80c62ce76cbb82f521d9
randyvroegop <randy.vroegop@luminis.eu>
2023-11-23 11:47:01 +0100

    Change apitolambda to lambdarestapi

--- lib/conf-cdk-stack.ts
+++ lib/conf-cdk-stack.ts
@@ -3,33 +3,33 @@ import {RemovalPolicy} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import {LambdaToDynamoDB} from '@aws-solutions-constructs/aws-lambda-dynamodb';
 import {Code, Runtime} from 'aws-cdk-lib/aws-lambda';
-import {ApiGatewayToLambda} from '@aws-solutions-constructs/aws-apigateway-lambda';
-import { Cors } from 'aws-cdk-lib/aws-apigateway';
+import {AuthorizationType, Cors, LambdaRestApi} from 'aws-cdk-lib/aws-apigateway';
 
 export class ConfCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props: cdk.StackProps) {
+        super(scope, id, props);
 
-    const lambdaToDynamo = new LambdaToDynamoDB(this, 'LambdaToDynamoPattern', {
-      lambdaFunctionProps: {
-        code: Code.fromAsset(`lambda`),
-        runtime: Runtime.NODEJS_18_X,
-        handler: 'index.handler'
-      },
-    });
+        const lambdaToDynamo = new LambdaToDynamoDB(this, 'LambdaToDynamoPattern', {
+            lambdaFunctionProps: {
+                code: Code.fromAsset(`lambda`),
+                runtime: Runtime.NODEJS_18_X,
+                handler: 'index.handler',
+            },
+        });
 
-    new ApiGatewayToLambda(this, 'ApiGatewayToLambdaPattern', {
-      existingLambdaObj: lambdaToDynamo.lambdaFunction,
-      apiGatewayProps: {
-        proxy: true,
-        defaultCorsPreflightOptions: {
-          allowOrigins: Cors.ALL_ORIGINS,
-          allowMethods: Cors.ALL_METHODS,
-          allowHeaders: ['*'],
-        },
-      }
-    });
+        new LambdaRestApi(this, 'ApiGatewayToLambdaPattern', {
+            handler: lambdaToDynamo.lambdaFunction,
+            proxy: true,
+            defaultCorsPreflightOptions: {
+                allowOrigins: Cors.ALL_ORIGINS,
+                allowMethods: Cors.ALL_METHODS,
+                allowHeaders: ['*'],
+            },
+            defaultMethodOptions: {
+                authorizationType: AuthorizationType.NONE
+            }
+        });
 
-    lambdaToDynamo.dynamoTable.applyRemovalPolicy(RemovalPolicy.DESTROY);
-  }
+        lambdaToDynamo.dynamoTable.applyRemovalPolicy(RemovalPolicy.DESTROY);
+    }
 }
